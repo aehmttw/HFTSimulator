c76cdd7186c49a187a3d96b31db65ffa7969510b
Aehmttw <mateibudiu@hotmail.com>
2021-02-25 22:29:50 -0800

    Made agents more modular

--- agents.py
+++ agents.py
@@ -3,38 +3,72 @@ from order import *
 from events import *
 import random
 class Agent:
-    def __init__(self, name: str, simulation: 'Simulation'):
-        self.balance = 10000.0
+    def __init__(self, name: str, simulation: 'Simulation', balance: float, shares: dict):
+        self.balance = balance
         self.name = name
         self.simulation = simulation
         
         # Symbol -> amount
-        self.shares = dict()
-        self.shares["A"] = 100
-
-        self.baseLatency = 0
+        self.shares = shares
 
         self.orderBooks = dict()
         self.sharePrices = dict()
+
+        self.algorithm = None
+        self.latencyFunction = None
      
     def inputData(self, trade: 'Trade', timestamp: int):
         self.sharePrices[trade.symbol] = trade.price
-        # How to handle un-matched trades?
-        
+        orders = self.algorithm.getOrders(trade.symbol, timestamp)
 
-    # Returns an Order telling what to do, or noOrder if nothing should be done
-    # Make sure to set the order timestamp based on the latency
-    def trade(self): # Limit order book as input or trades (market data)
-        raise NotImplementedError #instead of return add to queue
+        for order in orders:
+            self.simulation.pushEvent(EventOrder(timestamp + self.getLatency(), order, self.simulation.orderbooks[trade.symbol]))
+
     
-    # Returns a latency for an action
-    # If latency varies for each action by a bit, that variation can be added here to a base latency
-    # Maybe add other latencies to separate places too, depending on how this will be implemented
+# market maker - could schedule events at for self
+# maybe plot event queue size over time to see how long to simulate for, to see practicality
+
+class Algorithm:
+    # This class defines an algorithm that can be used by an agent 
+    def __init__(self, agent: Agent):
+        self.agent = agent
+
+    # returns a list of orders to place
+    def getOrders(self, symbol: str, timestamp: int):
+        raise NotImplementedError
+
+class AlgorithmFixedPrice(Algorithm):
+    # This class defines an algorithm that can be used by an agent 
+    def __init__(self, agent: Agent, price: float, symbol: list, quantity: int, buy: bool):
+        super().__init__(agent)
+        self.price = price
+        self.symbol = symbol
+        self.quantity = quantity
+        self.buy = buy
+
+    # returns a list of orders to place
+    def getOrders(self, symbol: str, timestamp: int):
+        order = Order(self, self.buy, symbol, self.quantity, self.price, timestamp)
+        return [order]
+
+class LatencyFunction:
+    # This class defines a latency distribution function that can be used by an agent
+    def __init__(self, agent: Agent):
+        self.agent = agent
+
+    # returns a list of orders to place
     def getLatency(self) -> int:
         raise NotImplementedError
 
-# market maker - could schedule events at for self
-# maybe plot event queue size over time to see how long to simulate for, to see practicality
+class LatencyFunctionLinear(LatencyFunction):
+    # Linear latency function: latency is linearly between min and max parameters
+    def __init__(self, agent: Agent, min: int, max: int):
+        super().__init__()
+        self.minLatency = min
+        self.maxLatency = max
+
+    def getLatency(self) -> int:
+        return random.randint(self.minLatency, self.maxLatency)
 
 class AgentFixedPrice(Agent):
     def __init__(self, name: str, simulation: 'simulation', price: float, symbol: list, quantity: int, buy: bool):
@@ -57,11 +91,6 @@ class AgentFixedPrice(Agent):
     def trade(self, symbol: str, timestamp: int):
         order = Order(self, self.buy, symbol, self.quantity, self.price, timestamp)
         self.simulation.pushEvent(EventOrder(timestamp + self.getLatency(), order, self.simulation.orderbooks[symbol]))
-
-
-
-
-
 
 # todo - add multiple agent types
 
