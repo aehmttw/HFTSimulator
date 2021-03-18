ce849f1d04207246115e94f25e2ca60955309570
Aehmttw <mateibudiu@hotmail.com>
2021-03-17 21:59:37 -0700

    Added multithreading for mass simulations

--- tester.py
+++ tester.py
@@ -5,11 +5,17 @@ from orderbook import *
 from trade import *
 from simulation import *
 from tests import *
+from multiprocessing import Process
 
-for i in range(100):
-    print("Running simulation " + str(i))
-    name: str = "test"
+name: str = "testmultithread"
+
+def runSimulation(num: int):
+    print("Running simulation " + str(num))
     simulation = Simulation("runs/" + name + "/simulation.json")
     simulation.run()
+    simulation.orderbooks["A"].write("runs/" + name + "/output" + str(num) + ".csv")
 
-    simulation.orderbooks["A"].write("runs/" + name + "/output" + str(i) + ".csv")
+for i in range(100):
+    p = Process(target=runSimulation, args=(i,))
+    p.start()
+    
